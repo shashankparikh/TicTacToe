d441618e9c6369f66f3686a5c8e49360d7db08ef
Shashank Parikh <bitsparikh@gmail.com>
2020-02-02 23:36:44 +0530

    tic tac code

--- Game.js
+++ Game.js
@@ -1,86 +1,204 @@
-import React, {useState, useEffect} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
-
-const GRID_LENGTH = 3;
-let turn = 'X';
-
-const Game = () => {
-  const [grid, setGrid] = useState([]);
-
-  const initializeGrid = () => {
-    const tempGrid = [];
-    for (let colIdx = 0; colIdx < GRID_LENGTH; colIdx++) {
-      const tempArray = [];
-      for (let rowidx = 0; rowidx < GRID_LENGTH; rowidx++) {
-        tempArray.push(0);
-      }
-      tempGrid.push(tempArray);
-    }
-    setGrid(tempGrid);
-  };
-
-  const getBox = (index, colIdx, rowIdx) => {
-    let backgroundColor = 'red';
-    const sum = colIdx + rowIdx;
-    if (sum % 2 === 0) {
-      backgroundColor = 'blue';
-    }
-    const gridValue = grid[colIdx][rowIdx];
-    let content = '-';
-    if (gridValue === 1) {
-      content = 'X';
-    } else if (gridValue === 2) {
-      content = 'O';
-    }
-    return (
-      <View key={index} styles={{...styles.boxStyle, backgroundColor}}>
-        <Text>{content}</Text>
-      </View>
-    );
-  };
-
-  const getRow = (row, colIdx) => {
-    return row.map((item, index) => {
-      return getBox(index, colIdx, index);
-    });
-  };
-
-  const getColumns = () => {
-    return grid.map((row, index) => {
-      return (
-        <View style={styles.rowStyle} key={index}>
-          {getRow(row, index)}
-        </View>
-      );
-    });
-  };
-
-  const renderMainGrid = () => {
-    return (
-      <View style={{display: 'flex'}}>
-        <View style={styles.columnsStyle}>{getColumns()}</View>
-      </View>
-    );
-  };
-
-  useEffect(() => {
-    initializeGrid();
-  }, []);
-
-  if (grid.length === 0) {
-    return <Text> initializing </Text>;
+import React, {Component} from 'react'
+import {StyleSheet, View, TouchableOpacity, Alert, Button} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+//import {styles} from './style.css'
+export default class home extends Component {
+  state = {
+    gameState: [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ],
+    currentPlayer: 1,
   }
-  return renderMainGrid();
-};
+
+  componentDidMount () {
+    this.initializeGame()
+  }
+
+  initializeGame = () => {
+    this.setState({
+      gameState: [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+      ],
+      currentPlayer: 1,
+    })
+  }
+
+  renderIcon = (row, col) => {
+    let value = this.state.gameState[row][col]
+
+    switch (value) {
+      case 1:
+        return <Icon name='close' style={styles.cross} />
+      case -1:
+        return <Icon name='circle-o' style={styles.zero} />
+      default:
+        return <View />
+    }
+  }
+
+  onTilePress = (row, col) => {
+    const {currentPlayer, gameState} = this.state
+
+    let value = gameState[row][col]
+    if (value !== 0) {
+      return
+    }
+
+    const arr = gameState.slice()
+    arr[row][col] = currentPlayer
+
+    let nextPlayer = currentPlayer === 1 ? -1 : 1
+    this.setState({gameState: arr, currentPlayer: nextPlayer})
+
+    let winner = this.getWinner()
+    if (winner === 1) {
+      setTimeout(()=>{Alert.alert('Player 1 is a winner')}, 500)
+
+      this.initializeGame()
+    } else if (winner === -1) {
+      setTimeout(()=>{Alert.alert('Player 2 is a winner')}, 500)
+      this.initializeGame()
+    }
+  }
+
+  getWinner = () => {
+    const tileNum = 3
+    let sum
+    let arr = this.state.gameState.slice()
+
+    // To check rows
+    for (let i = 0; i < tileNum; i++) {
+      sum = arr[i][0] + arr[i][1] + arr[i][2]
+      if (sum === 3) {
+        return 1
+      } else if (sum === -3) {
+        return -1
+      }
+    }
+
+    // To check col
+    for (let i = 0; i < tileNum; i++) {
+      sum = arr[0][i] + arr[1][i] + arr[2][i]
+      if (sum === 3) {
+        return 1
+      } else if (sum === -3) {
+        return -1
+      }
+    }
+
+    // To check Diagnol
+    sum = arr[0][0] + arr[1][1] + arr[2][2]
+    if (sum === 3) {
+      return 1
+    } else if (sum === -3) {
+      return -1
+    }
+
+    sum = arr[0][2] + arr[1][1] + arr[2][0]
+    if (sum === 3) {
+      return 1
+    } else if (sum === -3) {
+      return -1
+    }
+    //  For no winners
+    return 0
+  }
+
+  newGame = () => {
+    this.initializeGame()
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity
+            style={[styles.tile, {borderTopWidth: 0, borderLeftWidth: 0}]}
+            onPress={() => this.onTilePress(0, 0)}>
+            {this.renderIcon(0, 0)}
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.tile, {borderTopWidth: 0}]}
+            onPress={() => this.onTilePress(0, 1)}>
+            {this.renderIcon(0, 1)}
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.tile, {borderTopWidth: 0, borderRightWidth: 0}]}
+            onPress={() => this.onTilePress(0, 2)}>
+            {this.renderIcon(0, 2)}
+          </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity
+            style={[styles.tile, {borderLeftWidth: 0}]}
+            onPress={() => this.onTilePress(1, 0)}>
+            {this.renderIcon(1, 0)}
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.tile, {}]}
+            onPress={() => this.onTilePress(1, 1)}>
+            {this.renderIcon(1, 1)}
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.tile, {borderRightWidth: 0}]}
+            onPress={() => this.onTilePress(1, 2)}>
+            {this.renderIcon(1, 2)}
+          </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity
+            style={[styles.tile, {borderBottomWidth: 0, borderLeftWidth: 0}]}
+            onPress={() => this.onTilePress(2, 0)}>
+            {this.renderIcon(2, 0)}
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.tile, {borderBottomWidth: 0}]}
+            onPress={() => this.onTilePress(2, 1)}>
+            {this.renderIcon(2, 1)}
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.tile, {borderBottomWidth: 0, borderRightWidth: 0}]}
+            onPress={() => this.onTilePress(2, 2)}>
+            {this.renderIcon(2, 2)}
+          </TouchableOpacity>
+        </View>
+        <View style={styles.button}>
+          <Button title='New Game' onPress={this.newGame} />
+        </View>
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  columnsStyle: {
-    flexDirection: 'column',
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'yellow',
   },
-  rowStyle: {
-    flexDirection: 'row',
+  tile: {
+    borderWidth: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
   },
-  boxStyle: {width: 100, height: 100},
-});
-
-export default Game;
+  cross: {
+    flex: 1,
+    color: 'red',
+    fontSize: 80,
+  },
+  zero: {
+    flex: 1,
+    color: 'green',
+    fontSize: 80,
+  },
+  button: {
+    padding: 40,
+  },
+})
---
+++ style/theme.js
@@ -0,0 +1,9 @@
+
+const Theme = {
+    backgroundColor: {
+        primaryColor: "red", // darkblue
+    },
+    fontFamily: "Roboto_regular"
+}
+
+export default Theme;
